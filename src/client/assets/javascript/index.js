6b44865f2fb3e39b9e9d08b5dcf33170196f05ca
hikmanisyariful <syarifulfajar017@gmail.com>
2021-11-14 15:36:15 +0700

    Revision : set player_id in handleSelectPodRacer()

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -146,7 +146,7 @@ function handleSelectPodRacer(target) {
   target.classList.add("selected");
 
   // TODO - save the selected racer to the store
-  store.race_id = target.id;
+  store.player_id = target.id;
 }
 
 function handleSelectTrack(target) {
